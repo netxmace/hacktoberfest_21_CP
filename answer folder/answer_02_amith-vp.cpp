3e8d599c10b3b2b44e8c961206277c45e119d332
Amith V Purushothaman <66194856+amith-vp@users.noreply.github.com>
2021-09-30 23:20:14 +0530

    answer 2

---
+++ answer folder/answer_02_amith-vp.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t,n[1000000],f,l,i,j[1000000];
+    cout<<"Enter number of test cases \n";
+    cin>>t;
+    cout<<"Enter numbers \n";
+    while(t<=1000 && t>=1){
+        for(i=0;i<t;i++){
+            cin>>n[i];
+            f=n[i];
+            l=f%10;
+            while(f>=10){
+                f=f/10;
+            }
+            j[i]=l+f;
+        }
+    cout<<"\n"<<"sum"<<"\n";
+    for(i=0;i<t;i++){
+    cout<<j[i]<<"\n";
+    }
+    }
+    return 0;
+}
+  
