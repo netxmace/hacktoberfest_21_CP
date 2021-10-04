6f356154bfa937cacfc7f1b266545f318051cde7
Navaneeth Prabha <76899241+Navaneethp007@users.noreply.github.com>
2021-10-04 23:50:43 +0530

    Create answer_01_Navaneethp007.cpp

---
+++ answer folder/answer_01_Navaneethp007.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,s,t;
+    cin>>t;
+  
+    for(int i=0;i<t;i++){
+   
+      cin>>a;
+      b=a;
+      sum=b%10;
+    
+    for(int i=0;a>10;a++){
+        
+      a=a/10;
+    }
+    
+    sum=sum+a-1;
+    if(b<10){
+        
+      cout<<b<<"\n";
+    }
+    else  
+      cout<<sum<<"\n";}
+}
