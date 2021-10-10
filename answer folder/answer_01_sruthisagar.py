8d20df3e6d8b449a54ed96a281e8ddf93d02c58a
rachelkarengreen <73185208+rachelkarengreen@users.noreply.github.com>
2021-10-10 16:30:50 +0530

    uploaded answer3

--- answer folder/answer_01_sruthisagar.py
+++ answer folder/answer_01_sruthisagar.py
@@ -1,4 +1,4 @@
-# Question 2 (Print difference of 2 numbers if the first number is greater than the second number otherwise print their sum)
+# Question 1 (Print difference of 2 numbers if the first number is greater than the second number otherwise print their sum)
 
 n1 = int(input())
 n2 = int(input())
