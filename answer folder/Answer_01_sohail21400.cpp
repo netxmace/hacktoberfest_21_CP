f1723965fd54158e58baf2c79240a1eefa96e7c5
Sohail <sohail21400@gmail.com>
2021-10-14 16:21:25 +0530

    Create Answer_01_sohail21400.cpp

---
+++ answer folder/Answer_01_sohail21400.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int a=0,b=0;
+
+    cin>>a>>b;
+
+    if(a>b)
+
+       cout<<a-b<<endl;
+
+    else
+
+       cout<<a+b<<endl;
+
+return 0;
+
+}
